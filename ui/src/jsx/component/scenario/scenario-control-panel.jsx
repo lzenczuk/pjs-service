7e72aefe61eb3dbd3a33fdd76ad49e690043c8dc
Lucjan Zenczuk <lzenczuk@xanadu.ie>
2015-10-22 18:00:42 +0100

    Rename scenario top bar

--- ui/src/jsx/component/scenario/scenario-control-panel.jsx
+++ ui/src/jsx/component/scenario/scenario-control-panel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default
-class ScenarioDndPanel extends React.Component {
+class ScenarioControlPanel extends React.Component {
 
     render() {
         console.log("ScenarioDndPanel: render");
