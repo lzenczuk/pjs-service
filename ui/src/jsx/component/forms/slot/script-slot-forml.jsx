c056935ca4632d57aa713ece7f645e565600bd22
Lucjan Zenczuk <lzenczuk@xanadu.ie>
2015-11-27 14:57:24 +0000

    Add  changing slot label in script slots.

--- ui/src/jsx/component/forms/slot/script-slot-forml.jsx
+++ ui/src/jsx/component/forms/slot/script-slot-forml.jsx
@@ -3,6 +3,10 @@ import ctx from '../../../context';
 
 export default class ScriptSlotForm extends React.Component {
 
+    onLabelChange(event) {
+        ctx.scenarioActions.changeSlotLabel(this.props.nodeId, this.props.index, event.target.value);
+    }
+
     render(){
 
         let slotModel = this.props.slotModel;
@@ -10,7 +14,7 @@ export default class ScriptSlotForm extends React.Component {
         return (
             <div>
                 <div>Script slot</div>
-                <input className="name-form" type="text" value={slotModel.label} />
+                <input className="name-form" type="text" value={slotModel.label} onChange={this.onLabelChange.bind(this)}/>
                 <textarea className="script-form" value={slotModel.script} />
                 <div>{slotModel.desNodeId}</div>
                 <hr/>
